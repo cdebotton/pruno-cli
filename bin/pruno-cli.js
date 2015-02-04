d77efd2f24b69fdf913c041e3dde0269b2230d15
Christian de Botton <debotton@brooklynunited.com>
2015-02-04 16:36:00 -0500

    Cleaning up coloring on help.

--- bin/pruno-cli.js
+++ bin/pruno-cli.js
@@ -25,18 +25,18 @@ program.on("--help", function () {
   console.log("  " + "Generators:".yellow.underline.bold);
   console.log("");
   console.log("    " + "React:".yellow.underline.bold);
-  console.log("      " + "$".green + " pruno g " + "react:component".red + " " + "<componentName>".yellow);
-  console.log("      " + "$".green + " pruno g " + "react:actions".red + " " + "<actionsName>".yellow);
-  console.log("      " + "$".green + " pruno g " + "react:store".red + " " + "<storeName>".yellow);
-  console.log("      " + "$".green + " pruno g " + "react:mixin".red + " " + "<mixinName>".yellow);
+  console.log("      " + "☁".cyan + " pruno g " + "react:component".green.bold + " " + "<componentName>".yellow.bold);
+  console.log("      " + "☁".cyan + " pruno g " + "react:actions".green.bold + " " + "<actionsName>".yellow.bold);
+  console.log("      " + "☁".cyan + " pruno g " + "react:store".green.bold + " " + "<storeName>".yellow.bold);
+  console.log("      " + "☁".cyan + " pruno g " + "react:mixin".green.bold + " " + "<mixinName>".yellow.bold);
   console.log("");
   console.log("    " + "Backbone:".yellow.underline.bold);
-  console.log("      " + "$".green + " pruno g " + "backbone:router".red + " " + "<routerName>".yellow);
-  console.log("      " + "$".green + " pruno g " + "backbone:view".red + " " + "<viewName>".yellow);
-  console.log("      " + "$".green + " pruno g " + "backbone:template".red + " " + "<templateName>".yellow);
-  console.log("      " + "$".green + " pruno g " + "backbone:model".red + " " + "<modelName>".yellow);
-  console.log("      " + "$".green + " pruno g " + "backbone:collection".red + " " + "<collectionName>".yellow);
-  console.log("      " + "$".green + " pruno g " + "backbone:helper".red + " " + "<helperName>".yellow);
+  console.log("      " + "☁".cyan + " pruno g " + "backbone:router".green.bold + " " + "<routerName>".yellow.bold);
+  console.log("      " + "☁".cyan + " pruno g " + "backbone:view".green.bold + " " + "<viewName>".yellow.bold);
+  console.log("      " + "☁".cyan + " pruno g " + "backbone:template".green.bold + " " + "<templateName>".yellow.bold);
+  console.log("      " + "☁".cyan + " pruno g " + "backbone:model".green.bold + " " + "<modelName>".yellow.bold);
+  console.log("      " + "☁".cyan + " pruno g " + "backbone:collection".green.bold + " " + "<collectionName>".yellow.bold);
+  console.log("      " + "☁".cyan + " pruno g " + "backbone:helper".green.bold + " " + "<helperName>".yellow.bold);
 
 });
 
