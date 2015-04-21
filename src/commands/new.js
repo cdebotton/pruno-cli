ae93774308d8c053d35e7f3b28ef608e6949ea79
Christian de Botton <cdebotton@gmail.com>
2015-04-20 21:05:25 -0400

    Removing server functionality.

--- src/commands/new.js
+++ src/commands/new.js
@@ -36,33 +36,6 @@ let init = (program) => program.command("new")
       name: "config",
       message: "Where would you like to store your app config yaml files?",
       default: "./config"
-    }, {
-      type: "confirm",
-      name: "koa",
-      message: "Are you building a backend?",
-      default: false
-    }, {
-      type: "input",
-      name: "api",
-      message: "Where would you like to store your backend files?",
-      default: "./api"
-    }, {
-      type: "confirm",
-      name: "db",
-      message: "Would you like to use a database?",
-      default: false,
-      when(answers) {
-        return answers.koa;
-      }
-    }, {
-      type: "list",
-      name: "dbType",
-      message: "What type of database would you like to use?",
-      choices: ["Postgres", "MySQL", "MariaDB", "SQLite", "MSSQL"],
-      default: "Postgres",
-      when(answers) {
-        return answers.db;
-      }
     }], params => {
       log("Initializing pruno.");
       // Create .prunorc file
@@ -84,57 +57,6 @@ let init = (program) => program.command("new")
       scaffolder(params.type, params);
 
       saveDev(["pruno", "gulp"]);
-
-      if (params.koa) {
-        Generator.serverRegister(params);
-        Generator.server(params);
-
-        save([
-          "koa",
-          "koa-json",
-          "koa-compress",
-          "koa-bodyparser",
-          "koa-json",
-          "koa-static",
-          "koa-isomorphic",
-          "koa-router",
-          "koa-mount",
-          "require-dir",
-          "react-router",
-          "alt"
-        ]);
-      }
-
-      if (params.db) {
-        Generator.sequelizerc(params);
-        Generator.dbAssets(params);
-
-        save([
-          "sequelize",
-          "sequelize-cli",
-          "yaml-env-config",
-          "object-assign",
-          "babel"
-        ]);
-
-        switch (params.dbType) {
-          case "Postgres":
-            save(["pg", "pg-hstore"]);
-            break;
-          case "MySQL":
-            save(["mysql"]);
-            break;
-          case "MariaDB":
-            save(["mariasql"]);
-            break;
-          case "SQLite":
-            save(["sqlite3"]);
-            break;
-          case "MSSQL":
-            save(["tedius"]);
-            break;
-        }
-      }
     });
   });
 
